75b38108b23adc7d16affe13d41d90d3a0b88fe9
Evgeniy Zalevskiy <2600@ukr.net>
2019-08-16 17:07:20 +0300

    fixed tests

---
+++ tests/TemplateRenderTest.php
@@ -0,0 +1,8 @@
+<?php
+use ContributorsPlugin\View\TemplateRender;
+
+class TemplateRendreTest extends \WP_UnitTestCase{
+    public function testFirst(){
+        $this->assertEquals(1,1);
+    }
+}
